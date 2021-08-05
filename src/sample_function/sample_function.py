711206e162a76b3ec1ba1037db7ba0f238313194
Oleg Grytsynevych <oleggry@amazon.com>
2021-08-05 11:40:46 +0200

    put the echo timestamp in its own message field

--- src/sample_function/sample_function.py
+++ src/sample_function/sample_function.py
@@ -12,8 +12,10 @@ def function_handler(event, context):
 
     response = json.loads(event)
 
-    # Add the time we processed the message to our response
-    response['processed_at'] = str(datetime.now())
+    # Add the time we processed the message to our response (with failure)
+    # response['processed_at'] = str(datetime.now())
+    # the working one
+    response['echo_processed_at'] = str(datetime.now())
 
     response_string = json.dumps(response)
 
